a7c11da1574e44422bfb25dde04da8e4f0b6a981
salvatoredambrosio891-ai <salvatoredambrosio891@gmail.com>
2026-01-24 14:06:56 +0100

    Update config.js
    666

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 
 global.owner = [
-  ['393773842461', '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲', true],
-  ['xxxxxxxxxx'], //mettete il vostro numero al posto delle x e copiate sopra il formato dopo ovvero 'nome', true
+  ['393336413960', 'Salvatore', true],
+  ['19184025124', 'Simone', true],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],

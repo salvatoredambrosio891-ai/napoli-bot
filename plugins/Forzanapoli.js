b478d9fcb9780aa421e2df2c76fe71f2ff78d488
salvatoredambrosio891-ai <salvatoredambrosio891@gmail.com>
2026-03-15 20:38:12 +0100

    Create Forzanapoli.js
    666

---
+++ plugins/Forzanapoli.js
@@ -0,0 +1,20 @@
+// plugins/forzaNapoli.js
+let handler = async (m, { conn }) => {
+  try {
+    let forzaNapoli = `
+*Forza Napoli, sempre avanti💙⚽!*
+`;
+
+    return conn.sendMessage(m.chat, { text: forzaNapoli });
+
+  } catch (e) {
+    console.error(e);
+    return conn.sendMessage(m.chat, { text: '❌ Errore nell\'invio del grido!' });
+  }
+};
+
+handler.help = ['forzaNapoli']
+handler.tags = ['napoli']
+handler.command = /^\.forzaNapoli$/i; // qui il punto è incluso
+
+export default handler;
